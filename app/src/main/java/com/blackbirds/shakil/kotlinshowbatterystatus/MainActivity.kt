f1f8d6f5e6696246702b1cce7130b8f8f5f35e89
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-02-17 15:02:25 -0500

    Initial commit

---
+++ app/src/main/java/com/blackbirds/shakil/kotlinshowbatterystatus/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.blackbirds.shakil.kotlinshowbatterystatus
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var txtBatteryStatus: TextView? = null //1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        txtBatteryStatus = findViewById(R.id.txtBatteryStatus)
+
+        registerReceiver(this.mBatteryInfoReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))//3
+    }
+
+    private val mBatteryInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() //2
+    {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val level = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL,-1)
+            val scale = intent!!.getIntExtra(BatteryManager.EXTRA_SCALE,-1)
+
+            val batteryPct = level * 100 / scale.toFloat()
+
+            txtBatteryStatus!!.text = "Battery Status: $batteryPct%"
+        }
+    }
+}
